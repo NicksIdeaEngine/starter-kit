f7b04ec0a68235e3879f8c9034ea43a43213641c
Nick Garcia <nicksideaengine@gmail.com>
2019-10-22 12:32:25 -0400

    Minor tweaks to test out travis CI

--- src/index.test.js
+++ src/index.test.js
@@ -19,5 +19,5 @@ describe('index.html', () => {
     const h1 = window.document.getElementsByTagName('h1')[0];
     expect(h1.innerHTML).to.equal("Hello world!?");
     window.close();
-  })
-})
+  });
+});
